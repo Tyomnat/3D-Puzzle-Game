e828d152e2045ecf18623f5c498ff2c49bccbf3c
Kristijonas Karalaitis <kristijonas.karalaitis@gmail.com>
2021-02-23 22:49:05 +0200

    implemented bride

---
+++ 3DPuzzleGame/Assets/Scripts/BridgeDrop.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeDrop : MonoBehaviour
+{
+    [SerializeField]
+    GameObject box;
+    [SerializeField]
+    GameObject holder1;
+    [SerializeField]
+    GameObject holder2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == box)
+        {
+            Destroy(holder1);
+            Destroy(holder2);
+        }
+    }
+}
