e098dad1989596bd531693356cb6393ea3791223
Kristijonas Karalaitis <kristijonas.karalaitis@gmail.com>
2021-02-24 20:20:16 +0200

    F

--- 3DPuzzleGame/Assets/Scripts/TakeCube.cs
+++ 3DPuzzleGame/Assets/Scripts/TakeCube.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class TakeCube : MonoBehaviour
 {
+    Collider player = null;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,26 @@ public class TakeCube : MonoBehaviour
     void Update()
     {
         
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("touch");
+        if (other.gameObject.tag == "Player")
+        {s
+            player = other;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+        this.transform.position = player.gameObject.transform.position;
+        this.transform.parent = player.gameObject.transform;
+    }
+
+    private void OnMouseUp()
+    {
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
     }
 }
