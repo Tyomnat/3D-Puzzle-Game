0cf0453ba2f56ae4267b22e2cc77844316d7fad3
unknown <kristijonas.karalaitis@gmail.com>
2021-04-04 01:30:32 +0300

    added redstone

---
+++ 3DPuzzleGame/Assets/Scripts/Redstone/PullLever.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullLever : MonoBehaviour
+{
+    Animator animator;
+    bool isPulled = false;
+
+    public KeyCode key;
+    public Transform playerTouch = null;
+    public GameObject leverCollider = null;
+    bool isColliding = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (leverCollider.GetComponent<Collider>().bounds.Contains(playerTouch.position))
+        {
+            isColliding = true;
+        }
+        else
+        {
+            isColliding = false;
+        }
+
+        if (Input.GetKeyDown(key) && isColliding)
+        {
+            ChangeLeverState();
+        }
+    }
+
+    void ChangeLeverState()
+    {
+        isPulled = !isPulled;
+        animator.SetBool("open", isPulled);
+    }
+}
---
+++ 3DPuzzleGame/Assets/Scripts/Redstone/TurnOffRedstone.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOffRedstone : MonoBehaviour
+{
+    public bool defaultState = false;
+    public Material offMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!defaultState)
+        {
+            GetComponent<Renderer>().material = offMaterial;
+            var ps = GetComponentInChildren<ParticleSystem>();
+            ps.Stop();
+        }             
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
