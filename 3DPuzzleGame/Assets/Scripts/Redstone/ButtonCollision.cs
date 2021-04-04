3c280f9eeb0f759a7a4f08fe30892a5722876ea9
unknown <kristijonas.karalaitis@gmail.com>
2021-04-04 23:51:51 +0300

    redstone level done

--- 3DPuzzleGame/Assets/Scripts/Redstone/ButtonCollision.cs
+++ 3DPuzzleGame/Assets/Scripts/Redstone/ButtonCollision.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class ButtonCollision : MonoBehaviour
 {
+    public GameObject greenLamp;
+    public GameObject redLamp;
+    public GameObject wire;
     public GameObject cube;
     public Material greenColor;
+    TurnOffRedstone[] offRedStoneScripts;
 
     bool updated = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        offRedStoneScripts = wire.GetComponentsInChildren<TurnOffRedstone>();
+        foreach (var script in offRedStoneScripts)
+        {
+            script.state = false;
+        }
+        greenLamp.GetComponent<Renderer>().enabled = false;
+        greenLamp.GetComponentInChildren<Light>().enabled = false;
     }
 
     // Update is called once per frame
@@ -26,6 +36,14 @@ public class ButtonCollision : MonoBehaviour
         {
             updated = true;
             GetComponent<Renderer>().material = greenColor;
+            redLamp.GetComponent<Renderer>().enabled = false;
+            redLamp.GetComponentInChildren<Light>().enabled = false;
+            greenLamp.GetComponent<Renderer>().enabled = true;
+            greenLamp.GetComponentInChildren<Light>().enabled = true;
+            foreach (var script in offRedStoneScripts)
+            {
+                script.state = true;
+            }
         }
     }
 
