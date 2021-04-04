d26af79d8ee35373717783af54285a26c4dba1cc
unknown <kristijonas.karalaitis@gmail.com>
2021-04-04 13:42:01 +0300

    added redstone torch

---
+++ 3DPuzzleGame/Assets/Scripts/Redstone/RedstoneTorch.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedstoneTorch : MonoBehaviour
+{
+    public Material onMaterial;
+    public Material offMaterial;
+    public GameObject wire = null;
+    public bool torchState = true;
+    bool lastState = true;
+    ParticleSystem ps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponentInChildren<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (wire != null)
+        {
+            torchState = !wire.GetComponent<TurnOffRedstone>().GetState();
+        }
+        if (lastState != torchState)
+        {
+            lastState = torchState;
+            UpdateState();
+        }
+    }
+
+    void UpdateState()
+    {
+        if (torchState)
+        {
+            GetComponent<Renderer>().material = onMaterial;
+            ps.Play();
+        }
+        else
+        {
+            GetComponent<Renderer>().material = offMaterial;
+            ps.Stop();
+        }
+    }
+
+    public bool GetState()
+    {
+        return torchState;
+    }
+}
