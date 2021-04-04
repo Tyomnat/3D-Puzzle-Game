b020ac1dcf444aa48fd865d11802621418606f7f
unknown <kristijonas.karalaitis@gmail.com>
2021-04-04 16:07:31 +0300

    pushing block

---
+++ 3DPuzzleGame/Assets/Scripts/Redstone/OpenDoor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public GameObject redstone;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (redstone.GetComponent<TurnOffRedstone>().GetState())
+        {
+            animator.SetBool("open", true);
+        }        
+    }
+}
