8353cb7224ff2bb34bceff4fbf8a5f4b64dd67e6
unknown <kristijonas.karalaitis@gmail.com>
2021-04-04 19:51:13 +0300

    applied materials

---
+++ 3DPuzzleGame/Assets/Scripts/Redstone/ButtonCollision.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonCollision : MonoBehaviour
+{
+    public GameObject cube;
+    public Material greenColor;
+
+    bool updated = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!updated)
+        {
+            updated = true;
+            GetComponent<Renderer>().material = greenColor;
+        }
+    }
+
+    public bool GetState()
+    {
+        return updated;
+    }
+}
--- 3DPuzzleGame/Assets/Scripts/Redstone/PistonPush.cs
+++ 3DPuzzleGame/Assets/Scripts/Redstone/PistonPush.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PistonPush : MonoBehaviour
 {
+    public GameObject redstone;
+    Animator animator;
+    bool state = false;
+    bool lastState = false;
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody>().AddForce(Vector3.left * 10.0f);
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        state = redstone.GetComponent<TurnOffRedstone>().GetState();
+        if (state != lastState)
+        {
+            lastState = state;
+            GetComponent<Animator>().SetBool("extend", state);
+        }
     }
 }
