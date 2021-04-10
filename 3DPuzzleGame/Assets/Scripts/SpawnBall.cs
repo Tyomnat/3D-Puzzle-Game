cc660db6b6bebfc01bb11c1163b6d41e4df0ef8d
Tomas Petrauskas <Tyomnat@users.noreply.github.com>
2021-04-10 22:42:57 +0300

    Bouncy room and slope design
    Added room with bouncy balls and level ending slope design. Sliding to be implemented and level continuation.

---
+++ 3DPuzzleGame/Assets/Scripts/SpawnBall.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBall : MonoBehaviour
+{
+    public GameObject Ballprefab;
+
+    public Vector3 center;
+    public Vector3 size;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawningBall()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
+
+            Instantiate(Ballprefab, pos, Quaternion.identity);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1,0,0,0.5f);
+        Gizmos.DrawCube(center, size);
+    }
+}
--- 3DPuzzleGame/Assets/Scripts/UI.cs
+++ 3DPuzzleGame/Assets/Scripts/UI.cs
@@ -6,19 +6,26 @@ using UnityEngine.UI;
 public class UI : MonoBehaviour
 {
     //public GameObject player;
+    SpawnBall ballSpawn;
+    bool ballsSpawned = false;
     public GameObject uiText;
     static int score = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        ballSpawn = FindObjectOfType(typeof(SpawnBall)) as SpawnBall;
         GetComponentInChildren<ParticleSystem>().Stop();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (score == 3 && !ballsSpawned)
+        {
+            ballSpawn.SpawningBall();
+            ballsSpawned = true;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
