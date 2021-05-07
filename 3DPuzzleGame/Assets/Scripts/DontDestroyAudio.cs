bcdef1faf0e97cdc4525a417ac622395a7e5def0
kristutis <kristijonas.karalaitis@gmail.com>
2021-05-07 15:04:12 +0300

    options for effect sounds

--- 3DPuzzleGame/Assets/Scripts/DontDestroyAudio.cs
+++ 3DPuzzleGame/Assets/Scripts/DontDestroyAudio.cs
@@ -1,11 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DontDestroyAudio : MonoBehaviour
 {
+    public GameObject effectsSlider = null;
+    float volume = 1f;
     private void Awake()
     {
         DontDestroyOnLoad(this);
+    }
+
+    private void Update()
+    {
+        if (effectsSlider != null)
+        {
+            if (this.name == "EffectsSoundVolume")
+            {
+                volume = effectsSlider.GetComponent<Slider>().value;
+            }
+        }
+    }
+
+    public float GetEffectsVolume()
+    {
+        return volume;
     }
 }
---
+++ 3DPuzzleGame/Assets/Scripts/SoundEffectMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundEffectMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject volumeObject = GameObject.Find("EffectsSoundVolume");
+        if (volumeObject != null)
+        {
+            SetVolumes(volumeObject.GetComponent<DontDestroyAudio>().GetEffectsVolume());
+        } 
+        else
+        {
+            SetVolumes(1f);
+        }        
+    }
+
+    void SetVolumes(float volume)
+    {
+        GameObject[] coinSounds = GameObject.FindGameObjectsWithTag("PartySound");
+        AudioSource[] audiosources = GetComponentsInChildren<AudioSource>();
+        foreach (var audiosource in audiosources)
+        {
+            audiosource.volume = volume;
+        }
+        foreach (var coinSound in coinSounds)
+        {
+            coinSound.GetComponent<AudioSource>().volume = volume;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
