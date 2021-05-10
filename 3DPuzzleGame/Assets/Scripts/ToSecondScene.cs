11a881c1db2451c7fbb5415617fe373ec3ee0c12
unknown <kristijonas.karalaitis@gmail.com>
2021-05-10 14:31:48 +0300

    NNN

--- 3DPuzzleGame/Assets/Scripts/ToSecondScene.cs
+++ 3DPuzzleGame/Assets/Scripts/ToSecondScene.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ToSecondScene : MonoBehaviour
 {
+    bool respawned = false;
+    public GameObject text;
+    public float time = 0;
+
     private void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        text.SetActive(true);
+        text.GetComponent<Text>().text = "Time in level: " + time;
+        respawned = true;
     }
     // Start is called before the first frame update
     void Start()
@@ -18,6 +26,16 @@ public class ToSecondScene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        time += Time.deltaTime;
+        StartCoroutine(CoUpdate());
+    }
+
+    IEnumerator CoUpdate()
+    {
+        if (respawned)
+        {
+            yield return new WaitForSeconds(5);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 }
